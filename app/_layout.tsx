741767ad4c51d6c9e30380ab72fdaf453e3f5def
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2026-05-22 10:08:38 +0100

    Remove the default white bar of the android and remove that light when we navigate

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,23 @@
 import AuthProvider from "@/contexts/AuthContext";
+import { setBackgroundColorAsync } from "expo-system-ui";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
+setBackgroundColorAsync("#0d0d0d");
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <AuthProvider>
         <StatusBar style="light" />
-        <Stack screenOptions={{ headerShown: false }} />
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: "#0d0d0d" },
+            animation: "fade",
+          }}
+        />
       </AuthProvider>
     </SafeAreaProvider>
   );
